f9f5f2ef7a8a2d9d46c563d006bda44663cdfcd1
ElifAkin11 <165787747+ElifAkin11@users.noreply.github.com>
2024-04-02 14:20:06 +0300

    Add files via upload
    adding index html, scriptjs&stylecss

---
+++ script2.js
@@ -0,0 +1,7 @@
+<div id="messageContainer"></div>
+
+<script>
+document.getElementById("submitButton").addEventListener("click", function() {
+    document.getElementById("messageContainer").innerHTML = "Öneri için teşekkürler";
+});
+</script>
